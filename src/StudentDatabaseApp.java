6df60eeb8dd885bb79546f9dcac6a9becf292271
Ozan <ozannyurekli99@gmail.com>
2024-10-30 23:11:43 +0200

    Finished

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class StudentDatabaseApp {
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+
+
+    public static void main(String[] args) {
+        System.out.println("how much students you wanna add :");
+        Scanner input = new Scanner(System.in);
+        int numOfStudents = input.nextInt();
+        Student [] students = new Student [numOfStudents];
+        for (int i = 0 ; i<numOfStudents ; i++ ){
+            students[i] = new Student();
+            students[i].setId();
+            students[i].enrollCourses();
+            students[i].payTuition();
+            students[i].toString();
+            System.out.println("student added successfully \n");
+
+
+        }
+
+    }
+
 }
